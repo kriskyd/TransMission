f4a58375e404fd77a7a0584e44cc482d9c4ecbc6
Krzysztof Dziembowski <kkdziembowski@gmail.com>
2018-01-28 11:18:27 +0100

    dźwięki dla koła

---
+++ GeometryClash/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // standard
+    public AudioClip [] startGry, wstep, startRundy1, startRundy2, startRundy3, koniecWalki;
+
+
+    void Start ()
+    {
+
+    }
+
+    void Update ()
+    {
+
+    }
+}
